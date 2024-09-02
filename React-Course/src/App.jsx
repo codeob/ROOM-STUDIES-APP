2535c4990af724f2f755a3785d4168700974797a
obed <tobed1586@gmail.com>
2024-09-02 10:34:16 -0800

    obed

--- React-Course/src/App.jsx
+++ React-Course/src/App.jsx
@@ -11,7 +11,9 @@ function App() {
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque quia, molestiae alias fugit voluptate, iusto quidem quibusdam laudantium nihil, consequuntur quod facere autem quae fuga corrupti eligendi consectetur culpa. Quam harum nobis odit sint libero magni expedita dicta quisquam blanditiis numquam fugiat facere exercitationem vitae, accusantium voluptates minima repellat sit labore nam atque? At, necessitatibus corrupti. Quae minus at praesentium quisquam saepe repellat esse itaque quos, asperiores voluptate, fugit repudiandae commodi quia deserunt optio quasi aliquam debitis qui libero animi minima quibusdam quod! Incidunt eveniet fugiat dignissimos magni sed distinctio magnam ut labore eum. Ipsum similique accusantium sit minus odit quos corporis consectetur eum accusamus distinctio, fugit necessitatibus porro hic non inventore esse unde culpa quidem quam. Incidunt, perspiciatis dolorem ipsa ad facilis esse. Excepturi corporis nesciunt beatae expedita? Inventore repellendus recusandae rem maiores doloribus ullam nam unde, adipisci dignissimos iure repudiandae sunt quibusdam aliquam fugiat aliquid sed saepe sit suscipit ea nisi. Veniam, maiores consequuntur, et impedit eum 
         </div>
         <div className='bg-amber-500 relative '>
-          <div className='absolute bottom-0 right-0 bg-gray-800 h-11 w-36'></div>
+          <div className='absolute bottom-0 right-0 bg-gray-800 h-11 w-36'>
+            uuuu
+          </div>
         </div>
       </div>
     </div>
